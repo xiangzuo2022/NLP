3e199db5aff985c95c80b817c718074c62d7b31b
xiangzuo2022 <xiangzuo2012@gmail.com>
2022-04-10 23:07:36 -0700

    update April 10th 2022

---
+++ deep_learning/cnn.py
@@ -0,0 +1,135 @@
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+from torchvision import utils
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+cwd = os.getcwd()
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+#add CIFAR10 data in the environment
+sys.path.append(cwd + 'cifar10')
+
+#Numpy is linear algebra lbrary
+# Matplotlib is a visualizations library
+#CIFAR10 is a custom Dataloader that loads a subset ofthe data from a local folder
+batch_size = 4
+
+
+def show_image(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def load_data():
+
+    #convert the images to tensor and normalized them
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = CIFAR10(root='cifar10',  transform=transform, download=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=False, num_workers=1)
+    return trainloader
+
+#1. DEFINE THE CNN HERE
+"""
+A conv layer with 3 channels as input, 6 channels as output, and a 5x5 kernel
+A 2x2 max-pooling layer
+A conv layer with 6 channels as input, 16 channels as output, and a 5x5 kernel
+A linear layer with 1655 nodes
+A linear layer with 120 nodes
+A linear layer with 84 nodes
+A linear layer with 10 nodes
+"""
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+#2. TRAIN THE MODEL HERE
+def train(model, training_data):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    running_loss = 0.0
+
+    for epoch in range(1):  # loop over the dataset multiple times
+
+        for i, data in enumerate(training_data, 0):
+            # get the inputs; cifar10 is a list of [inputs, labels]
+            inputs, label = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            output = model(inputs)
+            loss = criterion(output, label)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+def evaluate(model):
+    dataiter = iter(load_data())
+    images, labels = dataiter.next()
+
+    # print images
+    show_image(utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' %
+            classes[labels[j]] for j in range(4)))
+
+    outputs = model(images)
+
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                    for j in range(4)))
+
+
+def main():
+
+    training_data = load_data()
+
+    model = CNN()
+
+    train(model, training_data)
+
+    evaluate(model)
+
+
+main()
--- emoji_transfer_learning.py
+++ emoji_transfer_learning.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import emoji as emoji
 import os 
+from tensorflow.python.keras.utils import to_categorical
+from tensorflow.python.keras.layers import *
+from tensorflow.python.keras.models import Sequential
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 print('Imported Successfully!')
@@ -34,3 +37,48 @@ with open('/newDisk/users/zhoumu/glove.6B.50d.txt',encoding='utf-8') as f:
 
 print(embeddings['eat'])
 print(embeddings['play'])
+
+
+XT = train[0]
+Xt = test[0]
+
+YT = to_categorical(train[1])
+Yt = to_categorical(test[1])
+
+
+print(XT.shape)
+print(Xt.shape)
+print(YT.shape)
+print(Yt.shape)
+
+def getOutputEmbeddings(X):
+    
+    embedding_matrix_output = np.zeros((X.shape[0],10,50))
+    for ix in range(X.shape[0]):
+        X[ix] = X[ix].split()
+        for jx in range(len(X[ix])):
+            embedding_matrix_output[ix][jx] = embeddings[X[ix][jx].lower()]
+            
+    return embedding_matrix_output
+
+emb_XT = getOutputEmbeddings(XT)
+emb_Xt = getOutputEmbeddings(Xt)
+print(emb_XT.shape)
+print(emb_Xt.shape)
+
+model = Sequential()
+model.add(LSTM(64,input_shape=(10,50),return_sequences=True))
+model.add(Dropout(0.4))
+model.add(LSTM(64,input_shape=(10,50)))
+model.add(Dropout(0.3))
+model.add(Dense(5))
+model.add(Activation('softmax'))
+model.summary()
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
+model.fit(emb_XT,YT,batch_size=32,epochs=40,shuffle=True,validation_split=0.1)
+model.evaluate(emb_Xt,Yt)
+pred = model.predict_classes(emb_Xt)
+for i in range(5):
+    print(''.join(Xt[i]))
+    print(emoji.emojize(emoji_dictionary[str(np.argmax(Yt[i]))]))
+    print(emoji.emojize(emoji_dictionary[str(pred[i])]))
